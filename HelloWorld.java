e271c4bf6ffaf4fc09446e9eec1ad3de4b50f474
zeta <1272195294@qq.com>
2022-06-05 15:39:42 +0800

    冲突合并在hot-fix分支上的修改

--- HelloWorld.java
+++ HelloWorld.java
@@ -3,6 +3,7 @@ public class HellWold{
 		System.out.println("hello world");
 		System.out.println("hello");
 		System.out.println("hot-fix分支的内容");
+		System.out.println("在Hot-fix分支上又添加的内容");
 	}
 
 }
