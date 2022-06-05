62f50153b8105b9ffc3acb0812b88fff72648fd5
zeta <1272195294@qq.com>
2022-06-05 15:37:24 +0800

    冲突合并在master分支上的修改

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,8 +1,9 @@
 public class HellWold{
 	public static void main(String[] args){
 		System.out.println("hello world");
-		System.out.println("hello");
+		System.out.println("66");
 		System.out.println("hot-fix分支的内容");
+		System.out.println("在master分支上添加的内容");
 	}
 
 }
